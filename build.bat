e4557cbd9a127714bf2683f12480da3d2310484c
fahadasq <fahad.a.s.algarni@gmail.com>
2024-08-01 02:23:27 +0300

    Added Screenshots

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-clang main.c -o tetris.exe -luser32 -lgdi32 --target=x86_64-pc-windows-msvc -g
-::clang main.c -o tetris.exe -luser32 -lgdi32 -O3
+::clang main.c -o tetris.exe -luser32 -lgdi32 --target=x86_64-pc-windows-msvc -g
+clang main.c -o tetris.exe -luser32 -lgdi32 -O3
